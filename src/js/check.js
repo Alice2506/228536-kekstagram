ecc7650e49001324898286312ad071bec37d7bfa
Alice2506 <ralko84@mail.ru>
2016-10-13 21:25:28 +0300

    правка файла check

--- src/js/check.js
+++ src/js/check.js
@@ -1,6 +1,47 @@
 function getMessage(a, b) {
-if (a = true) {return ('Переданное GIF-изображение анимировано и содержит [b] кадров');
+
+  if (typeof a === 'boolean') {
+
+    return getMessageForGif(a, b);
+
+  }
+
+  if (typeof a === 'number') {
+
+    return getMessageForSvg(a, b);
+
+  }
+
+  if (Array.isArray([a]) = true) {
+
+    return ('Количество красных точек во всех строках изображения: [amountOfRedPoints]');
+
+  }
+
+  if (Array.isArray([a, b]) = true) {
+
+    return ('Количество красных точек во всех строках изображения: [amountOfRedPoints]');
+
+  }
 }
-else {return ('Переданное GIF-изображение не анимировано');
+
+
+
+function getMessageForGif(a, b) {
+
+  if (a === true) {
+
+    return ('Переданное GIF-изображение анимировано и содержит [b] кадров');
+
+  } else {
+
+    return ('Переданное GIF-изображение не анимировано');
+
+  }
+
 }
+
+
+function getMessageForSvg(a, b) {
+  return ('Переданное сообщение содержит [a] объектов и [b * 4] атрибутов');
 }
