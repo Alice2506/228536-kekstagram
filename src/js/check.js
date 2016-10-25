f7f516fdf9684b17953bd5d1e42aea47bd02d602
Alice2506 <ralko84@mail.ru>
2016-10-25 22:43:24 +0300

    правка

--- src/js/check.js
+++ src/js/check.js
@@ -1,53 +1,34 @@
 function getMessage(a, b) {
-
   if (typeof a === 'boolean') {
-
     return getMessageForGif(a, b);
-
   }
 
   if (typeof a === 'number') {
-
     return getMessageForSvg(a, b);
-
   }
 
   if (Array.isArray(a)) {
-
-    return ('Количество красных точек во всех строках изображения: [amountOfRedPoints]');
-
+    return 'Количество красных точек во всех строках изображения: ' + amountOfRedPoints(a, b);
   }
 
-  if (Array.isArray(a, b)) {
-
-    return ('Количество красных точек во всех строках изображения: [amountOfRedPoints]');
-
+  if (Array.isArray(a) && Array.isArray(b)) {
+    return 'Общая площадь артефактов сжатия: ' + artifactsSquare(a, b) + ' пикселей';
   }
 
   else {
-
-    return ('Переданы некорректные данные');
-    
+    return 'Переданы некорректные данные';
   }
 }
-
-
 
 function getMessageForGif(a, b) {
-
   if (a === true) {
-
-    return ('Переданное GIF-изображение анимировано и содержит [b] кадров');
+    return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
 
   } else {
-
-    return ('Переданное GIF-изображение не анимировано');
-
+    return 'Переданное GIF-изображение не анимировано';
   }
-
 }
 
-
 function getMessageForSvg(a, b) {
-  return ('Переданное сообщение содержит [a] объектов и [b * 4] атрибутов');
+  return 'Переданное сообщение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов';
 }
